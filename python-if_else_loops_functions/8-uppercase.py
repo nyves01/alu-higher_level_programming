ab0ac728947e2d9e45f1882c3cc6a06d717d38ed
nyves01 <n.yves@alustudent.com>
2025-10-28 23:47:51 +0000

    uppercase character

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uppercase(str):
+    uppercase = 0
+    for x in str:
+        if ord(x) >= 97 and ord(x) <= 122:
+            uppercase = 32
+        else:
+            uppercase = 0
+        print("{}".format(chr(ord(x) - uppercase)), end='')
+    print()m
