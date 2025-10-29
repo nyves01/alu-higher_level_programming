a1ddb7d9be0e401cb0a0b7d0b0a764cbb845fdfe
nyves01 <n.yves@alustudent.com>
2025-10-29 14:35:23 +0000

    end digits

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
-def uppercase(str):
-    uppercase = 0
-    for x in str:
-        if ord(x) >= 97 and ord(x) <= 122:
-            uppercase = 32
-        else:
-            uppercase = 0
-        print("{}".format(chr(ord(x) - uppercase)), end='')
-    print()#!/usr/bin/python3
-def uppercase(str):
-    uppercase = 0
-    for x in str:
-        if ord(x) >= 97 and ord(x) <= 122:
-            uppercase = 32
-        else:
-            uppercase = 0
-        print("{}".format(chr(ord(x) - uppercase)), end='')
-    print()
+def print_last_digit(number):
+    last_dig = int(str(number)[-1])
+    print("{}".format(last_dig), end='')
+    return(last_dig))
